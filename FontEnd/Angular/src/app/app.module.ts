d15877e5e8dc2c7ac8cfcd28312e94fb1dbc92a2
binh.na@aris-vn.com <binh.na@aris-vn.com>
2019-07-08 18:08:35 +0700

    create file struct

--- FontEnd/Angular/src/app/app.module.ts
+++ FontEnd/Angular/src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChangeTextDirective } from './change-text.directive';
+import { AppBootstrapModule } from './app-bootstrap.module';
+
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+
+import { BsDropdownModule, TooltipModule, ModalModule } from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainpageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppBootstrapModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [BsDropdownModule, TooltipModule, ModalModule]
 })
 export class AppModule { }
